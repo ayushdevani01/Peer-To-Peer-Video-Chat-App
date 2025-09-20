149df405191c2740e28a4ef434a0d204c46d2c06
Ayush Devani <ayushdevani01@gmail.com>
2025-09-20 22:50:21 +0530

    change origin to check backend

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
   },
 });
