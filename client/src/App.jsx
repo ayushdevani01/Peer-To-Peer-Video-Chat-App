305d5ee195db7d8c617ec538364e3724a4259ade
Ayush Devani <ayushdevani01@gmail.com>
2025-10-02 17:36:23 +0530

    Add turn server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -54,8 +54,16 @@ const App = () => {
       if (peerConnections.current[socketID]) return;
       
       const pc = new RTCPeerConnection({
-        iceServers: [{ urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"] }],
-      });
+    iceServers: [
+      { urls: "stun:stun.l.google.com:19302" },
+      { urls: "stun:stun1.l.google.com:19302" },
+      {
+        urls: 'turn:openrelay.metered.ca:80',
+        username: 'openrelayproject',
+        credentials: 'openrelayproject'
+      },
+    ],
+  });
 
       pc.onicecandidate = (event) => {
         if (event.candidate) {
